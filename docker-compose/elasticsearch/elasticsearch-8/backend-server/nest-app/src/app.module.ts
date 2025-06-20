926c5024e1c8d56c8b427c61ea97c647aa069890
Lee-Sang-Yun <sylee@ontacthealth.com>
2025-06-20 17:33:14 +0900

    add: apm-agent 옵션 추가

--- docker-compose/elasticsearch/elasticsearch-8/backend-server/nest-app/src/app.module.ts
+++ docker-compose/elasticsearch/elasticsearch-8/backend-server/nest-app/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApmModule } from './modules/apm.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ApmModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
